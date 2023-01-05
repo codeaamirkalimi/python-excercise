7af463633bceb81b0bf4cce883631a9796b05b48
Aamir Kalimi <codekalimi@gmail.com>
2023-01-05 13:19:59 +0530

    hackerrank list comprehension

--- hackerrank_list_comprehension.py
+++ hackerrank_list_comprehension.py
@@ -1,6 +1,27 @@
 # newlist = [expression for item in iterable if condition == True]
 
-ages = [1, 2, 3, 4, 5]
-print("Ages", ages)
-newAge = [age for age in ages if age > 2]
-print("New Age", newAge)
+# ages = [1, 2, 3, 4, 5]
+# print("Ages", ages)
+# newAge = [age for age in ages if age > 2]
+# print("New Age", newAge)
+x = int(input("x:"))
+y = int(input("y:"))
+z = int(input("z:"))
+n = int(input("n:"))
+
+# result = []
+# for i in range(x + 1):
+#     for j in range(y + 1):
+#         for k in range(z + 1):
+#             if x + y + z != n:
+#                 result.append([i, j, k])
+#
+# print(result)
+
+listCompResult = [[i, j, k]
+                  for i in range(x + 1)
+                  for j in range(y + 1)
+                  for k in range(z + 1)
+                  if (i + j + k) != n
+                  ]
+print(listCompResult)
