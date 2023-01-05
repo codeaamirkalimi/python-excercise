4227f73b2705a610752e091d5161db8c9a896bdd
Aamir Kalimi <codekalimi@gmail.com>
2023-01-05 12:58:07 +0530

    hackerrank list comprehension

---
+++ hackerrank_list_comprehension.py
@@ -0,0 +1,6 @@
+# newlist = [expression for item in iterable if condition == True]
+
+ages = [1, 2, 3, 4, 5]
+print("Ages", ages)
+newAge = [age for age in ages if age > 2]
+print("New Age", newAge)
