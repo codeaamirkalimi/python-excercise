8913ad6c2053025e9f341961c0607b25dd1d4fcc
Aamir Kalimi <aamir_k@ormae.com>
2023-01-24 23:39:05 +0530

    add new node to the head of the linkedlist

--- DSA/CreateNewLinkedList.py
+++ DSA/CreateNewLinkedList.py
@@ -7,3 +7,30 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def printLinkedList(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+
+    def addFirstNodeToLinkedList(self, data):
+        temp = self.head
+        self.head = Node(data)
+        self.head.next = temp
+
+
+if __name__ == "__main__":
+    llist = LinkedList()
+    llist.head = Node(0)
+    second = Node(1)
+    third = Node(3)
+    llist.head.next = second
+    second.next = third
+    llist.printLinkedList()
+    print("\n---------------------")
+    llist.addFirstNodeToLinkedList(4)
+    llist.printLinkedList()
+    print("\n---------------------")
+    llist.addFirstNodeToLinkedList(5)
+    llist.printLinkedList()
