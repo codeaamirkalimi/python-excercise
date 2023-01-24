226b09e022e3aa13083d762c44c57fb6885da37c
Aamir Kalimi <aamir_k@ormae.com>
2023-01-25 00:24:31 +0530

    delete value in the linkedlist

--- DSA/CreateNewLinkedList.py
+++ DSA/CreateNewLinkedList.py
@@ -38,6 +38,18 @@ class LinkedList:
                 checkNode = checkNode.next
         return False
 
+    def deleteNode(self, key):
+        checkNode = self.head
+        if checkNode.data == key:
+            self.head = self.head.next
+        else:
+            while checkNode.next is not None:
+                if checkNode.next.data == key:
+                    checkNode.next = checkNode.next.next
+                    break
+                else:
+                    checkNode = checkNode.next
+
 
 if __name__ == "__main__":
     llist = LinkedList()
@@ -61,4 +73,22 @@ if __name__ == "__main__":
     llist.printLinkedList()
     print("\n---------------------")
     print(llist.search(54))
+    llist.printLinkedList()
+    print("\n---------------------")
+    llist.deleteNode(5)
+    llist.printLinkedList()
+    print("\n---------------------")
+    llist.deleteNode(0)
+    llist.printLinkedList()
+    print("\n---------------------")
+    llist.deleteNode(8)
+    llist.printLinkedList()
+    print("\n---------------------")
+    llist.deleteNode(1)
+    llist.printLinkedList()
+    print("\n---------------------")
+    llist.deleteNode(3)
+    llist.printLinkedList()
+    print("\n---------------------")
+    llist.deleteNode(33)
     llist.printLinkedList()
