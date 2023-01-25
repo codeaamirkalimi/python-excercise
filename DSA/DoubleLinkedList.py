a601cc13ea20a389102a1ad7da3cac8c70a23ebb
Aamir Kalimi <aamir_k@ormae.com>
2023-01-25 13:29:34 +0530

    create DoubleLinkedList

---
+++ DSA/DoubleLinkedList.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, data):
+        self.prev = None
+        self.data = data
+        self.next = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def printDoublyLinkedList(self):
+        global last
+        temp = self.head
+        print("Forward Printing\n----------------\n")
+        while temp is not None:
+            print(temp.data, end=" ")
+            if temp.next is None:
+                last = temp
+            temp = temp.next
+        print("\nBackward Printing\n---------------\n")
+        temp = last
+        while temp is not None:
+            print(temp.data, end=" ")
+            temp = temp.prev
+
+
+if __name__ == "__main__":
+    doublyLinkedList = DoublyLinkedList()
+    firstNode = Node(10)
+    secondNode = Node(20)
+    thirdNode = Node(30)
+    fourthNode = Node(60)
+    doublyLinkedList.head = firstNode
+    firstNode.next = secondNode
+    secondNode.prev = firstNode
+    secondNode.next = thirdNode
+    thirdNode.prev = secondNode
+    thirdNode.next = fourthNode
+    fourthNode.prev = thirdNode
+
+    doublyLinkedList.printDoublyLinkedList()
